4b433883fa311114266d753635e93500dde2ba18
Booli <x700700@gmail.com>
2018-09-02 21:04:50 +0300

    auth logs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,14 @@
 const httpStatus = require('http-status');
 const APIError = require('./APIError');
+const logger = require('./logger');
 
 
 exports.requiresLogin = (req, res, next) => {
     if (req.isAuthenticated()) {
+        logger.info(`HTTP origin [${req.user && req.user.email}] req: ${req.baseUrl}`);
         return next();
     } else {
+        logger.error(`Unauthenticated request origin [${req.ip}] - req: ${req.baseUrl}`);
         return next(new APIError('You must first sign in', httpStatus.UNAUTHORIZED));
     }
 };
